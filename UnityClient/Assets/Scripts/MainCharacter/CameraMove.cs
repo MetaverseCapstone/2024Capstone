7e9ddc2bd00ce0d7102268467e8a7d3eee0a19de
Lee Hye Rim <20211924@edu.hanbat.ac.kr>
2024-01-18 21:44:21 +0900

    오브젝트 생성, 삭제 구현
    딜레이 있는 생성, 삭제 구현 + 인게임 UI 일부 수정 및 추가

--- UnityClient/Assets/Scripts/MainCharacter/CameraMove.cs
+++ UnityClient/Assets/Scripts/MainCharacter/CameraMove.cs
@@ -4,28 +4,41 @@ using UnityEngine;
 
 public class CameraMove : MonoBehaviour
 {
-    public float sensitivity = 200f;
-    float rotationX;
+    float sensitivity;
     float rotationY;
+
+    GameObject parentCharacter;
+
+    private void Awake()
+    {
+        // 부모 객체 (캐릭터) 가져오기
+        parentCharacter = this.transform.parent.gameObject;
+        // 캐릭터의 sensitivity를 그대로 가져옴
+        sensitivity = parentCharacter.GetComponent<CharacterMove>().sensitivity;
+    }
 
     void Start()
     {
         
     }
 
-    
     void Update()
     {
-        float mouseX = Input.GetAxis("Mouse X");
+        CameraRotationY();
+    }
+
+    void CameraRotationY()
+    {
         float mouseY = Input.GetAxis("Mouse Y");
 
-        rotationX += mouseX * sensitivity * Time.deltaTime;
         rotationY += mouseY * sensitivity * Time.deltaTime;
 
+        Vector3 parentAngle = parentCharacter.transform.eulerAngles;
+
         // 카메라를 위, 아래로 회전시킬 때 제한을 설정
-        rotationY = rotationY > 35f ? 35f : rotationY; 
+        rotationY = rotationY > 35f ? 35f : rotationY;
         rotationY = rotationY < -30f ? -30f : rotationY;
 
-        transform.eulerAngles = new Vector3(-rotationY, rotationX, 0);
+        transform.eulerAngles = new Vector3(-rotationY, parentAngle.y, 0);
     }
 }
