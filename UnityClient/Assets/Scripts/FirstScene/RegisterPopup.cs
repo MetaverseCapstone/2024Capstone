8c124b2dede92339100caf0cccec24611ba10869
Lee Hye Rim <20211924@edu.hanbat.ac.kr>
2024-01-24 20:47:30 +0900

    request 일부 부분 구현
    rebase 하기 전 커밋

--- UnityClient/Assets/Scripts/FirstScene/RegisterPopup.cs
+++ UnityClient/Assets/Scripts/FirstScene/RegisterPopup.cs
@@ -2,7 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Networking;
 using TMPro;
+
+public class UserInfo
+{
+    public string id;
+    public string email;
+    public string pw;
+}
 
 public class RegisterPopup : MonoBehaviour
 {
@@ -16,6 +24,8 @@ public class RegisterPopup : MonoBehaviour
     private string id;
     private string email;
     private string pw;
+
+    private string url = "http://localhost:4000";
 
     public void Awake()
     {
@@ -45,11 +55,71 @@ public class RegisterPopup : MonoBehaviour
 
     public void OnClickRegister()
     {
+        failText.gameObject.SetActive(false);
+        successfulTxt.gameObject.SetActive(false);
+
+        id = idField.text;
+        email = emailField.text;
+        pw = pwField.text;
+
         Debug.Log("Register Clicked");
+        Debug.Log("ID : " + id + " Email : " + email + " PW : " + pw);
+
+        UserInfo userInfo = new UserInfo
+        {
+            id = id,
+            email = email,
+            pw = pw,
+        };
+
+        string json = JsonUtility.ToJson(userInfo);
+
+
+        /*
+        if (!findUserID(id))
+        {
+            
+        }
+        else
+        {
+            failText.text = "This ID is already taken. Please enter a different ID.";
+            failText.gameObject.SetActive(true);
+        }
+        */
+        
     }
 
-    public bool findUserID(string id)
+    public bool findDuplicatet(string id, string email)
     {
-        return true;
+        string duplicateIDU = url + "/users/duplicate";
+
+        return false;
     }
+
+    IEnumerator RequestPost(string url, string json)
+    {
+        using(UnityWebRequest request = UnityWebRequest.PostWwwForm(url, json))
+        {
+            byte[] jsonToSend = new System.Text.UTF8Encoding().GetBytes(json);
+            request.uploadHandler = new UploadHandlerRaw(jsonToSend);
+            request.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
+            request.SetRequestHeader("Content-Type", "application/json");
+            yield return request.SendWebRequest();   
+        }
+    }
+
+    IEnumerator RequestGet(string url)
+    {
+        UnityWebRequest request = UnityWebRequest.Get(url);
+        yield return request.SendWebRequest();
+        if (request.result != UnityWebRequest.Result.Success)
+        {
+            Debug.Log(request.error);
+        }
+        else
+        {
+            Debug.Log(request.downloadHandler.text);
+        }
+    }
+
 }
